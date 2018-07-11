0b7493dc695f2e2c1c2255bfb9d33eb68111a832
DESKTOP-20Q2FQU\Emma Lemercier <emma.lemercier@ntymail.com>
2018-07-11 02:06:38 +0200

    Ajout héritage 2

--- src/Entity/PointDeVente.php
+++ src/Entity/PointDeVente.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PointDeVenteRepository")
  */
-class PointDeVente
+class PointDeVente extends Utilisateur
 {
     /**
      * @ORM\Id()
