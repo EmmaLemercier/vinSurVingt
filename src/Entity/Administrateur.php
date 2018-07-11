69d18267389f688012735d46ef7b1ad6658180b1
DESKTOP-20Q2FQU\Emma Lemercier <emma.lemercier@ntymail.com>
2018-07-11 02:05:49 +0200

    Ajout  héritage

--- src/Entity/Administrateur.php
+++ src/Entity/Administrateur.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdministrateurRepository")
  */
-class Administrateur
+class Administrateur extends Utilisateur
 {
     /**
      * @ORM\Id()
